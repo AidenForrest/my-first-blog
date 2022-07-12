efefec7126d0817847e95e484e258b31d5d89cb2
Aiden Forrest <aidenforrest2006@gmail.com>
2022-07-12 14:49:29 +0100

    Added comments and finsihed main functionality

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -7,5 +7,14 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'text')
         widgets = {
             'title': forms.TextInput(attrs={'style': 'border-top-width: 0px; border-left-width: 0px; border-right-width: 0px; outline: none; border-bottom-color: black; margin-bottom: 20px'}),
+            'text': forms.Textarea(attrs={'style': 'border-bottom-color: black; outline: none; width: 75vw;', 'placeholder': 'Enter post body'})
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
+        widgets = {
+            'author': forms.TextInput(attrs={'style': 'border-top-width: 0px; border-left-width: 0px; border-right-width: 0px; outline: none; border-bottom-color: black; margin-bottom: 20px'}),
             'text': forms.Textarea(attrs={'style': 'border-bottom-color: black; outline: none; width: 75vw;', 'placeholder': 'Enter post body'})
         }
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('accounts/login/', authViews.LoginView.as_view(), name='login'),
+    path('accounts/logout/', authViews.LogoutView.as_view(next_page='/'), name='logout'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
