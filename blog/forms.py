ee5390d5a04f062833d12a5a5a755e804e458f87
Aiden Forrest <aidenforrest2006@gmail.com>
2022-07-12 11:54:32 +0100

    Polished up version 1

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text')
+        widgets = {
+            'title': forms.TextInput(attrs={'style': 'border-top-width: 0px; border-left-width: 0px; border-right-width: 0px; outline: none; border-bottom-color: black; margin-bottom: 20px'}),
+            'text': forms.Textarea(attrs={'style': 'border-bottom-color: black; outline: none; width: 75vw;', 'placeholder': 'Enter post body'})
+        }
